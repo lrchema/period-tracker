3e2c696465d2fa8cb7564b6d0730291706065a06
lrchema <lrchema@gmail.com>
2023-10-12 23:20:05 +0800

    Persist list of seleected dates

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
+import './Calendar.css';
+import Calendar from 'react-calendar';
 import './App.css';
 
 function App() {
+  const [dates, setDates] = useState(JSON.parse(localStorage.getItem('dates')) ? JSON.parse(localStorage.getItem('dates')) : []);
+  useEffect(()=>{
+    localStorage.setItem('dates', JSON.stringify(dates))
+    console.log(dates);
+  },[dates]);
+
+  const handleChangeDates = (date) => {
+    var copy = dates.slice();
+    if (copy.includes(date.getTime())){
+      var index = copy.indexOf(date.getTime());
+      copy.splice(index, 1);
+      setDates(copy);
+    }
+    else {
+      copy.push(date.getTime());
+      setDates(copy);
+    }
+  }
   return (
     <div className="App">
+      <div className='calendar-container'>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Calendar 
+        onChange={handleChangeDates} 
+        tileClassName={({ date, view }) => {
+       // date will return every date visible on calendar and view will view type (eg. month)
+       if(dates.includes(date.getTime())){
+         return 'highlight'; // your class name
+        }
+      }}/>
+      
       </header>
+      </div>
     </div>
   );
 }
